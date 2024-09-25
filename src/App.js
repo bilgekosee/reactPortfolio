5c17a8400a5329afd01a2ae8ddb54829815af7b4
bilgekosee <bilge.koosee@gmail.com>
2024-09-25 16:24:55 +0300

    homeand header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
-/* import { HashRouter as Router, Routes, Route } from "react-router-dom"; */
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
